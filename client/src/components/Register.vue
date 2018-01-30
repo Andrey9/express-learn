e751f47534ba1d66ee846326118e99cd01086b27
Andrey Melnyk <andrey1397@gmail.com>
2018-01-30 12:13:28 +0200

    stared implementing auth

---
+++ client/src/components/Register.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>Register</h1>
+
+    <input type="email" name="email" placeholder="Email" v-model="email">
+    <br>
+    <input type="password" name="password" placeholder="Password" v-model="password">
+    <br>
+    <button @click="register">Register</button>
+  </div>
+</template>
+
+<script>
+import AuthService from '@/services/AuthService'
+export default {
+  name: 'register',
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    async register () {
+      await AuthService.register({
+        email: this.email,
+        password: this.password
+      })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
