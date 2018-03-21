7ba48c99124fd8c22b970bad00a9b5dc14700ba8
Andrey Melnyk <andrey1397@gmail.com>
2018-03-21 12:42:32 +0200

    fixed comments actions

--- server/src/app/controllers/CommentController.ts
+++ server/src/app/controllers/CommentController.ts
@@ -8,18 +8,19 @@ import { CommentValidator } from '../middleware/schemas/CommentValidator';
 export class CommentController implements IController {
   init (app: Application): void {
     app.post('/posts/:post_id/comments', auth, validateInput(CommentValidator.create), this.create);
-    app.post(
+    app.put(
       '/posts/:post_id/comments/:comment_id',
-      auth,
-      validateInput(CommentValidator.create),
-      this.update);
+      auth, validateInput(CommentValidator.create),
+      this.update
+    );
+    app.delete('/posts/:post_id/comments/:comment_id', auth, this.destroy);
   }
 
   private async create (req: Request, res: Response, next: NextFunction) {
     try {
       const post = await PostService.getOnePost(req.query.post_id);
       const user = req.user ? req.user : {};
-      const comment = await CommentService.create({ ...req.body, author: user._id }, post);
+      const comment = await CommentService.createComment({ ...req.body, author: user._id }, post);
       res.json(comment);
     } catch (error) {
       next(error);
@@ -28,8 +29,17 @@ export class CommentController implements IController {
 
   private async update (req: Request, res: Response, next: NextFunction) {
     try {
-      const comment = await CommentService.update(req.query.comment_id);
-      res.json({ comment });
+      const comment = await CommentService.updateComment(req.query.comment_id, req.body);
+      res.json(comment);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  private async destroy (req: Request, res: Response, next: NextFunction) {
+    try {
+      const comment = await CommentService.deleteComment(req.query.comment_id);
+      res.json(comment);
     } catch (error) {
       next(error);
     }
--- server/src/app/services/CommentService.ts
+++ server/src/app/services/CommentService.ts
@@ -4,11 +4,25 @@ import { IPostModel } from '../interfaces/models/IPost';
 import { HttpError } from '../errors/HttpError';
 
 export class CommentService {
-  public static async create (input: ICommentModel, post: IPostModel): Promise<ICommentModel>   {
+  public static async createComment (
+    input: ICommentModel,
+    post: IPostModel): Promise<ICommentModel>   {
     const comment = new Comment({ ...input, post: post._id });
 
     post.comments.push(comment);
     await post.save();
+
+    return comment.save();
+  }
+
+  public static async updateComment (id: string, input: ICommentModel): Promise<ICommentModel> {
+    const comment = await Comment.findOne({ id });
+
+    if (!comment) {
+      throw new HttpError(404, 'Comment not found');
+    }
+
+    comment.set(input);
 
     return comment.save();
   }
